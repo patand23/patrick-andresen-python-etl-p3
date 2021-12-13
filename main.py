5e6a906b94604e3eb251f51616357668318fe440
Patrick Andresen <patrickand23@gmail.com>
2021-12-13 10:09:00 -0600

    Monday cleanup

--- main.py
+++ main.py
@@ -5,21 +5,19 @@ from pgsql import _query
 if __name__ == '__main__':
 
     client = bigquery.Client()
-    query = client.query(
-        """
+    query = client.query("""
         SELECT geo_id, state, county, sales_vector FROM(
-            SELECT c.geo_id, m.sub_region_1 AS state, m.sub_region_2 AS county, avg(m.retail_and_recreation_percent_change_from_baseline) AS sales_vector
-            FROM `bigquery-public-data.census_bureau_acs.county_2017_1yr` c
-            JOIN `bigquery-public-data.covid19_google_mobility.mobility_report` m 
-            ON c.geo_id ||'.0' = m.census_fips_code
-            WHERE c.median_rent < 2000 AND c.median_age < 30
-            GROUP BY c.geo_id, state, county
-            ORDER BY geo_id DESC)
+            SELECT geo_id, sub_region_1 AS state, sub_region_2 AS county, AVG(retail_and_recreation_percent_change_from_baseline) AS sales_vector
+            FROM `bigquery-public-data.census_bureau_acs.county_2017_1yr`
+            JOIN `bigquery-public-data.covid19_google_mobility.mobility_report`
+            ON geo_id ||'.0' = census_fips_code
+            WHERE median_rent < 2000 AND median_age < 30
+            GROUP BY geo_id, state, county
+            ORDER BY geo_id)
         WHERE sales_vector > -15
-        """
-    )
-    for row in query.result():
-        _query(sql.insert_data, row)
+    """)
 
     _query(sql.create_schema)
     _query(sql.create_table)
+    for row in query.result():
+        _query(sql.insert_data, row)
